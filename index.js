1c0d32f4dce8b5842de7930b8abb4cd4ed11b2dc
theusaf <theusafyt@gmail.com>
2020-03-21 22:46:00 -0700

    Prepare electron

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-if(!process.versions.qode){
-  throw "Err! Started using node, please use npm start instead";
-}
-
 // Modules
 const {spawn,exec} = require("child_process");
 const path = require("path");
 const fs = require("fs");
-const {QProgressBar, QLabel, FlexLayout, QWidget, QMainWindow, QIcon, QMessageBox, ButtonRole, QPushButton} = require("@nodegui/nodegui");
 
 // app modules
 const isGitInstalled = require(path.join(__dirname,"src/DetectGit.js"));
@@ -18,7 +13,6 @@ const parts = require(path.join(__dirname,"parts.js"));
 const __ = screens.load();
 
 global.win = __[0];
-const progress = __[1];
 
 // Detect Stuff
 isGitInstalled(options.git).then(res=>{
@@ -31,15 +25,6 @@ isGitInstalled(options.git).then(res=>{
 });
 
 function startupExit(reason){
-  const m = new QMessageBox();
-  m.setText("Error: " + reason);
-  const ok = new QPushButton();
-  ok.setText("ok");
-  m.addButton(ok,ButtonRole.AcceptRole);
-  m.exec();
-  process.exit(0);
 }
 function startupLoad(){
-  progress.setValue(100);
-  screens.main(win,screens,parts);
 }
