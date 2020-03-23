61fd9c2d4f59700f366f328101eeb73cd53ee0a5
theusaf <theusafyt@gmail.com>
2020-03-23 16:38:35 -0700

    moved parts to data folder

--- src/js/parts.js
+++ src/js/parts.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 module.exports = function(part){
   return new Promise((res,rej)=>{
-    fs.readFile(path.join(__dirname,"../../screens/parts",part + ".html"),'utf8',(err,html)=>{
+    fs.readFile(path.join(__dirname,"../../data/parts",part + ".html"),'utf8',(err,html)=>{
       res(html);
     });
   });
