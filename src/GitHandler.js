516e0a923ec6cf68b37e252cc61f678402322a00
theusaf <theusafyt@gmail.com>
2020-03-22 20:22:16 -0700

    update code

--- src/GitHandler.js
+++ src/GitHandler.js
@@ -22,22 +22,37 @@ module.exports = {
       if(!fs.existsSync(path.join(__dirname,"../data/repos/",repo))){
         return rej(repo);
       }
-      const fetcher = spawn(git || "git",["fetch"],{
-        cwd: path.join(__dirname,"../data/repos/",repo)
-      });
+      const fetcher = spawn(git || "git",["fetch"],{cwd: path.join(__dirname,"../data/repos/",repo)});
       fetcher.stderr.on("data",err=>{
         rej(err);
       });
       fetcher.on("close",()=>{
         // checkout files, search for changes.
         let inf = [];
-        const changes = spawn(git || "git",["checkout"]);
+        let toDate = false;
+        const changes = spawn(git || "git",["checkout"],{cwd: path.join(__dirname,"../data/repos/",repo)});
         changes.stderr.on("data",err=>{rej(err)});
         changes.on("error",err=>{rej(err)});
         changes.stdout.on("data",data=>{
           data = data.toString();
-          if(data.search){
+          if(data.search("up to date with") != -1 && data[0].search(/[adm]/i) == -1){
+            toDate = true;
+          }else if(data[0].search(/[adm]/i) != -1){
+            inf.push(data.split("	")[1]);
+          }else if(data.search("is behind") != -1){
+            toDate = false;
+          }
+        });
+        changes.on("close",()=>{
+          if(toDate){
+            // commit any changes
+            if(inf.length){
 
+            }else{
+              res("updated");
+            }
+          }else{
+            // pull
           }
         });
       });
