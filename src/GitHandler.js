88013ebd0732f7898174b4f6076dbdd314daedd5
theusaf <theusafyt@gmail.com>
2020-03-22 20:12:27 -0700

    update git handler

--- src/GitHandler.js
+++ src/GitHandler.js
@@ -17,16 +17,45 @@ module.exports = {
       });
     });
   },
-  fetchChanges: git=>{
+  fetchChanges: (git,repo)=>{
+    return new Promise((res,rej)=>{
+      if(!fs.existsSync(path.join(__dirname,"../data/repos/",repo))){
+        return rej(repo);
+      }
+      const fetcher = spawn(git || "git",["fetch"],{
+        cwd: path.join(__dirname,"../data/repos/",repo)
+      });
+      fetcher.stderr.on("data",err=>{
+        rej(err);
+      });
+      fetcher.on("close",()=>{
+        // checkout files, search for changes.
+        let inf = [];
+        const changes = spawn(git || "git",["checkout"]);
+        changes.stderr.on("data",err=>{rej(err)});
+        changes.on("error",err=>{rej(err)});
+        changes.stdout.on("data",data=>{
+          data = data.toString();
+          if(data.search){
+
+          }
+        });
+      });
+      fetcher.on("error",error=>{
+        rej(error);
+      });
+    });
+  },
+  loadFiles: ()=>{
 
   },
-  uploadChanges: git=>{
+  uploadChanges: (git,repo)=>{
 
   },
-  hideFile: (git,file)=>{
+  hideFile: (git,repo,file)=>{
 
   },
-  showFile: (git,file)=>{
+  showFile: (git,repo,file)=>{
 
   }
 }
